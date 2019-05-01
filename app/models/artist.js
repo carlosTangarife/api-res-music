0ff78964816d1f0086943781198571bb944cfc2a
Carlos Javier Tangarife Gil <carlos.tangarife@ceiba.com.co>
2019-05-01 10:19:17 -0500

    the artist file was renamed from artist..js to artist.js

--- app/models/artist.js
+++ app/models/artist.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const Genre = require('../models/genre');
+const Genre = require('./genre');
 
 const ArtistsSchema = new Schema({
   name:{
