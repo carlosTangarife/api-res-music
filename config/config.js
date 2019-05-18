3f1576e88a6c1239b89ee9b37d4154de4679850d
carlosTangarife <cjt860826@hotmail.com>
2019-05-18 14:47:45 -0500

    se agrega llave secreta

--- config/config.js
+++ config/config.js
@@ -9,7 +9,8 @@ const config = {
       name: 'api-music'
     },
     port: process.env.PORT || 3000,
-    db: 'mongodb://carlos:c4r10s@clustercarlos-shard-00-00-loyt5.mongodb.net:27017,clustercarlos-shard-00-01-loyt5.mongodb.net:27017,clustercarlos-shard-00-02-loyt5.mongodb.net:27017/music?ssl=true&replicaSet=clusterCarlos-shard-0&authSource=admin&retryWrites=true'
+    db: 'mongodb://carlos:c4r10s@clustercarlos-shard-00-00-loyt5.mongodb.net:27017,clustercarlos-shard-00-01-loyt5.mongodb.net:27017,clustercarlos-shard-00-02-loyt5.mongodb.net:27017/music?ssl=true&replicaSet=clusterCarlos-shard-0&authSource=admin&retryWrites=true',
+    SECRET_TOKEN: '1 [3AW#.AMg{np7?BN!y+t:(1DH%$Bi,qvi:NBr1vr-H}=9(T|GwEszr+0mF~fV['
   },
 
   test: {
@@ -18,7 +19,8 @@ const config = {
       name: 'api-music'
     },
     port: process.env.PORT || 3000,
-    db: 'mongodb://carlos:c4r10s@clustercarlos-shard-00-00-loyt5.mongodb.net:27017,clustercarlos-shard-00-01-loyt5.mongodb.net:27017,clustercarlos-shard-00-02-loyt5.mongodb.net:27017/music?ssl=true&replicaSet=clusterCarlos-shard-0&authSource=admin&retryWrites=true'
+    db: 'mongodb://carlos:c4r10s@clustercarlos-shard-00-00-loyt5.mongodb.net:27017,clustercarlos-shard-00-01-loyt5.mongodb.net:27017,clustercarlos-shard-00-02-loyt5.mongodb.net:27017/music?ssl=true&replicaSet=clusterCarlos-shard-0&authSource=admin&retryWrites=true',
+    SECRET_TOKEN: '1 [3AW#.AMg{np7?BN!y+t:(1DH%$Bi,qvi:NBr1vr-H}=9(T|GwEszr+0mF~fV['
   },
 
   production: {
@@ -27,7 +29,8 @@ const config = {
       name: 'api-music'
     },
     port: process.env.PORT || 3000,
-    db: 'mongodb://carlos:c4r10s@clustercarlos-shard-00-00-loyt5.mongodb.net:27017,clustercarlos-shard-00-01-loyt5.mongodb.net:27017,clustercarlos-shard-00-02-loyt5.mongodb.net:27017/music?ssl=true&replicaSet=clusterCarlos-shard-0&authSource=admin&retryWrites=true'
+    db: 'mongodb://carlos:c4r10s@clustercarlos-shard-00-00-loyt5.mongodb.net:27017,clustercarlos-shard-00-01-loyt5.mongodb.net:27017,clustercarlos-shard-00-02-loyt5.mongodb.net:27017/music?ssl=true&replicaSet=clusterCarlos-shard-0&authSource=admin&retryWrites=true',
+    SECRET_TOKEN: '1 [3AW#.AMg{np7?BN!y+t:(1DH%$Bi,qvi:NBr1vr-H}=9(T|GwEszr+0mF~fV['
   }
 };
 
