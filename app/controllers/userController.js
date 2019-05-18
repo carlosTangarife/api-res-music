c849d9d13c708ceb9ac3124593817f43e4924527
carlosTangarife <cjt860826@hotmail.com>
2019-05-18 17:43:49 -0500

    se crea enpoint singin

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -40,3 +40,54 @@ router.get('/users', isAuth, (req, res, next) => {
     res.status(200).send({users});
   });
 });
+
+router.post('/singIn', (req, res) => {
+  let userRequest = new User({
+    email: req.body.email,
+    password: req.body.password
+  });
+
+  User.findOne({ email: userRequest.email.toLowerCase() }, (err, userFound) => {
+    if (err) {
+        return handlerError(res, err);
+    }
+
+    if(!userFound){
+      return handlerInvalidUser(res);
+    }
+
+    return comparePassword(userFound, userRequest, res);
+
+  }).select('_id email password');
+})
+
+function comparePassword(userFound, userRequest, res) {
+  return userFound.comparePassword(userRequest.password, (err, isMatch) => {
+    if (err) {
+      return handlerError(res, err);
+    }
+    if (!isMatch) {
+      return handlerInvalidUser(res);
+    }
+    return res.status(200).json({
+      ok: true,
+      token: jwt.createToken(userRequest),
+      message: 'user loged succesfully'
+    });
+  });
+}
+
+function handlerInvalidUser(res) {
+  return res.status(404).json({
+    ok: false,
+    message: 'invalid user',
+  });
+}
+
+function handlerError(res, err) {
+  return res.status(500).json({
+    ok: false,
+    message: 'error to singin' + err,
+  });
+}
+
