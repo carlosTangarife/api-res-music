7cf8adb576920893a79e603eabb35c90e7b92082
carlosTangarife <cjt860826@hotmail.com>
2019-05-18 14:48:49 -0500

    se crea entidad de usuario

---
+++ app/models/user.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt-nodejs');
+const crypto = require('crypto');
+
+const UserSchema = new Schema({
+  email: {
+    type: String,
+    unique: true,
+    lowercase: true,
+    required: true
+  },
+  displayName: String,
+  password: {
+      type: String,
+      select: false,
+      required: true
+  },
+  signupDate: {
+    type: Date,
+    default: Date.now(),
+    lastLogin: Date
+}
+});
+
+UserSchema.pre("save", function(next) {
+  bcrypt.genSalt(10, (err, salt) => {
+    if(err){
+      return next(err);
+    }
+    bcrypt.hash(this.password, salt, null, (err, hash) => {
+      if(err){
+        return next(err);
+      }
+      this.password = hash;
+      next()
+    })
+  })
+})
+
+
+UserSchema.methods.comparePassword = (candidatePassword, callback) => {
+  bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
+    callback(err, isMatch)
+  })
+}
+
+mongoose.model('User', UserSchema);
+
