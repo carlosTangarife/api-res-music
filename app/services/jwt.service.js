da49e0bc2e580160537cff689b1dc4f32e47f00d
carlosTangarife <cjt860826@hotmail.com>
2019-05-18 17:45:02 -0500

    por actualizacion del paquete de jwt, se debe quitar la validacion de la expiracion del token, esta ya esta incluida en la libreria

--- app/services/jwt.service.js
+++ app/services/jwt.service.js
@@ -18,18 +18,11 @@ function decodeToken (token) {
   const decoded = new Promise((resolve, reject) => {
     try {
       const payload = jwt.decode(token, config.SECRET_TOKEN)
-
-      if (payload.exp <= moment().unix()) {
-        reject({
-          status: 401,
-          message: 'The token was expired'
-        })
-      }
       resolve(payload.sub)
     } catch (err) {
       reject({
-        status: 500,
-        message: 'Invalid Token'
+        status: 401,
+        message: err.message
       })
     }
   })
